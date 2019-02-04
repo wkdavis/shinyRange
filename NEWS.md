762d052f744ed3e0b3819a768f70a949a6465d11
wkdavis <william.davis@worthingtonindustries.com>
2019-02-04 14:52:17 -0500

    added demo

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# shinyRange 0.0.2
+
+* New `demoNumericRange()` provides an interactive demonstration of 
+`numericRangeInput()`.  
+
 # shinyRange 0.0.1
 
-* Initial release.
+* Initial release.  
---
+++ R/demos.R
@@ -0,0 +1,23 @@
+#' And example showing how numericRangeInput works
+#'
+#' @name demos
+#' @export
+#'
+#' @importFrom shiny shinyAppFile
+#'
+#' @examples
+#' \dontrun{
+#'
+#' if (interactive()) {
+#'
+#' demoNumericRange()
+#'
+#' }
+#'
+#' }
+demoNumericRange <- function(example = "datepicker") {
+  shiny::shinyAppFile(
+    appFile = system.file(sprintf("examples/numericRange/app.R", example), package = "shinyRange"),
+    options = list("display.mode" = "showcase")
+  )
+}
